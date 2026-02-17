3538621ed1c254bfe8b986811f2535b28a224013
racostab <racostab4@gmail.com>
2026-02-17 11:38:21 -0600

    Cambio

--- Acosta_Bermejo_Raul/io_estandar.py
+++ Acosta_Bermejo_Raul/io_estandar.py
@@ -4,6 +4,6 @@ print("Salida estandar")
 dato1 = input("Dame el dato: ")
 dato2 = input()
 
-print("D1 ", dato1)
-print("D2 ".format(dato2))
+print("D1:", dato1)
+print("D2: {}".format(dato2))
       
