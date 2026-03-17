440864b7b4bd78d9d6a9bb82428c853159f27982
jruiza26-Maker <jruiza26@cic.ipn.mx>
2026-03-17 10:20:24 -0600

    Add files via upload
    El documento Ollama.py contiene el prompt escrito de la manera más sencilla y sin mucho contexto para la primera parte de "Sort with IA" y le pedi
    "Explica como funciona un algoritmo 'Counting sort'." dado que a mi me toco el algoritmo Counting sort.

---
+++ Ruiz-Ramirez-Juan_Pablo/Unidad 2 Basics of algorithms/Ollama.py
@@ -0,0 +1,36 @@
+#
+# Usando indirectamente el protocolo HTTP para interactuar con el modelo LLaMA a través de la API de Ollama.
+# Server 
+# OLLAMA_HOST="127.0.0.1:8080" 
+import requests
+import json
+
+local  = "localhost"
+remote = "100.113.158.78"
+
+urls = [ "http://"+ local  +":11434/api/generate",
+         "http://"+ remote +":11434/api/generate",
+       ]
+
+models = [ "llama3.2",
+           "qwen3:4B",
+           "gemma3:1b"
+         ]
+prompt = [ "3+4",
+           "Explica como funciona un algoritmo 'Counting sort'."
+         ]
+
+url = urls[0]
+payload = {
+    "model":  models[2],
+    "prompt": prompt[1],
+    "stream": False
+}
+
+response = requests.post(url, json=payload)
+
+if response.status_code == 200:
+    data = response.json()
+    print(data["response"])
+else:
+    print("Error:", response.status_code, response.text)
